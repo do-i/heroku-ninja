213a7065c1d1c945cddefbc67a6f0cfb3bc41982
Acorn Ninja <lupin@myway.com>
2012-10-13 12:03:08 -0500

    removed 1.7 only method

--- src/main/java/org/djd/fun/taiga/dao/DaoException.java
+++ src/main/java/org/djd/fun/taiga/dao/DaoException.java
@@ -23,8 +23,4 @@ public class DaoException extends Exception {
   public DaoException(Throwable cause) {
     super(cause);    //To change body of overridden methods use File | Settings | File Templates.
   }
-
-  protected DaoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-    super(message, cause, enableSuppression, writableStackTrace);    //To change body of overridden methods use File | Settings | File Templates.
-  }
 }
